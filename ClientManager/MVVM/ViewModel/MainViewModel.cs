57768a550c3e513bf3e62081eef8a3981d8a984a
Ildar <mirectape@gmail.com>
2023-06-18 19:04:40 +0500

    Basic ground for the project is complete

--- ClientManager/MVVM/ViewModel/MainViewModel.cs
+++ ClientManager/MVVM/ViewModel/MainViewModel.cs
@@ -9,7 +9,12 @@ namespace ClientManager.MVVM.ViewModel
 {
     class MainViewModel: ObservableObject
     {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+
         public HomeViewModel HomeVM { get; set; }
+        public DiscoveryViewModel DiscoveryVM { get; set; }
 
         private object _currentView;
 
@@ -26,7 +31,17 @@ namespace ClientManager.MVVM.ViewModel
         public MainViewModel()
         {
             HomeVM = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
             CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DiscoveryVM;
+            });
         }
     }
 }
