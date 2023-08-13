f9dff972040dfdef5f2888852a01a9aa31873eb4
Ildar <mirectape@gmail.com>
2023-08-13 17:49:10 +0500

    Bank VM & V bases got connected - still empty though

---
+++ ClientManager/ViewModels/BankWorkerViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ClientManager.Models;
+using ClientManager.Services;
+using ClientManager.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+using System.Windows.Data;
+
+namespace ClientManager.ViewModels
+{
+    class BankWorkerViewModel : WorkerViewModelBase
+    {
+        BankWorkerViewModel(Repository repository) : base(repository) 
+        {
+
+        }
+    }
+}
